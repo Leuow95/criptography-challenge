501403baefbf7a2ab5796b245d27d220d91c6efe
leonardomaia <leuow95@gmail.com>
2024-12-20 16:29:39 -0400

    transaction Repository

--- src/main/java/dev/leomaia/criptography_challenge/repositories/TransactionRepository.java
+++ src/main/java/dev/leomaia/criptography_challenge/repositories/TransactionRepository.java
@@ -2,6 +2,8 @@ package dev.leomaia.criptography_challenge.repositories;
 
 import dev.leomaia.criptography_challenge.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
 }
