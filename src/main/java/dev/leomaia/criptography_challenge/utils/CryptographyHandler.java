3cb08b2ffc1a3a4a7f98566b0857a615ea6e019b
leonardomaia <leuow95@gmail.com>
2024-12-20 17:17:48 -0400

    implemented cryptography handler

---
+++ src/main/java/dev/leomaia/criptography_challenge/utils/CryptographyHandler.java
@@ -0,0 +1,28 @@
+package dev.leomaia.criptography_challenge.utils;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.util.Base64;
+
+public class CryptographyHandler {
+
+    private static final String SECRET_KEY = "1234567890uyitjf";
+
+    public static String encrypt(String value)throws Exception{
+        SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedValue = cipher.doFinal(value.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedValue);
+    }
+
+    public static String decrypt(String value) throws Exception {
+        SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] decodedValue = Base64.getDecoder().decode(value);
+        byte[] decryptedValue = cipher.doFinal(decodedValue);
+        return new String(decryptedValue);
+    }
+}
