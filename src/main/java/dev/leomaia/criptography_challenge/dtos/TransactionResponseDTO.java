d138f789f89793c4ef809d3335c89f216459d40d
leonardomaia <leuow95@gmail.com>
2024-12-22 13:59:14 -0400

    created mapper to TransactionResponseDTO

--- src/main/java/dev/leomaia/criptography_challenge/dtos/TransactionResponseDTO.java
+++ src/main/java/dev/leomaia/criptography_challenge/dtos/TransactionResponseDTO.java
@@ -1,9 +1,20 @@
 package dev.leomaia.criptography_challenge.dtos;
 
+import dev.leomaia.criptography_challenge.entities.Transaction;
+
 public record TransactionResponseDTO(
         Long id,
         String userDocument,
         String creditCardToken,
         Long value
 ) {
+    public static TransactionResponseDTO fromEntity(Transaction transaction){
+        return new TransactionResponseDTO(
+                transaction.getId(),
+                transaction.getUserDocument(),
+                transaction.getCreditCardToken(),
+                transaction.getValue()
+
+        );
+    }
 }
